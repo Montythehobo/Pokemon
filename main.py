f8fd9a963968572698b9101563222df4b4d369e3
andrew <andrew.bannister@forwessun.com>
2021-10-08 13:43:46 +0100

    added typing

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Trainer: #defines the trainer and their attributes
     
     
 class Monster: #Defines the pokémon class and their attributes
-    def __init__(self, species, ivs, evs, level):#, moves, #ability,\
+    def __init__(self, species, ivs, evs, level, status):#, moves, #ability,\
                  #stats, #backslash tells program to carry to next line (for readability)\
                  #current_health, level, status, name, accuracy): #Defines important values needed
         
@@ -26,14 +26,14 @@ class Monster: #Defines the pokémon class and their attributes
         #self.ability = ability  #Ability will be related to species
 #        self.current_health = 1
         self.level = level
-#        self.status = "none"
+        self.status = status
 #        self.name = species
 #        self.accuracy = 100
         
         #stat order is Health, Attack, Defence, Special Attack, Special Defence, Speed
         #final ev spot is for max EVs, if total of other EVs = 256, will become 1 and no more EVs will be added
         
-    def attribs(self): #declares a list which will find base stats
+    def pk_stats(self): #declares a list which will find base stats
         base_stats = []
         stats = [1,1,1,1,1,1]
         f = open('mon_stats.txt') #opens the file with details about mons' stats
@@ -49,13 +49,28 @@ class Monster: #Defines the pokémon class and their attributes
             stats[i] = math.floor(math.floor(((math.floor(self.evs[i]/4)+self.ivs[i]+(2*base_stats[i]))*self.level)/100)+5)
         print(stats)
         
+    def pk_type(self):
+        type = []
+        f = open('mon_type.txt') #opens the file with details about mons' types
         
-monster1 = Monster(species="Pikachu", ivs = [0,0,0,0,0,0], evs = [0,0,0,0,0,0], level = 100)        
-monster2 = Monster(species="Caterpie", ivs = [0,0,0,0,0,0], evs = [0,0,0,0,0,0], level = 50)        
-monster3 = Monster(species="Garchomp", ivs = [24,12,30,16,23,5], evs = [74,190,91,48,84,23], level = 78)        
-monster4 = Monster(species="Pikachu", ivs = [20,10,7,14,30,1], evs = [10,20,30,40,50,60], level = 83)
-monster1.attribs()
-monster2.attribs()
-monster3.attribs()
-monster4.attribs()
+        for line in f:
+            if(line.strip() == self.species):
+                for ty in range (0,2):
+                    type.append(next(f).strip())
+                break
+        print(type)
+        
+monster1 = Monster(species="Pikachu", ivs = [0,0,0,0,0,0], evs = [0,0,0,0,0,0], level = 100, status = '')        
+monster2 = Monster(species="Caterpie", ivs = [0,0,0,0,0,0], evs = [0,0,0,0,0,0], level = 50, status = '')        
+monster3 = Monster(species="Garchomp", ivs = [24,12,30,16,23,5], evs = [74,190,91,48,84,23], level = 78, status = '')        
+monster4 = Monster(species="Pikachu", ivs = [20,10,7,14,30,1], evs = [10,20,30,40,50,60], level = 83, status = '')
+monster1.pk_stats()
+monster1.pk_type()
+monster2.pk_stats()
+monster2.pk_type()
+monster3.pk_stats()
+monster3.pk_type()
+monster4.pk_stats()
+monster4.pk_type()
+
 
